5761499bcd756d23e2e3566fc4afe61fdf87bb0f
Gabriel Nakaema <gsnakaema@gmail.com>
2021-04-18 18:45:45 -0300

    mock data loads into state variable

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,19 @@
 import { mockData } from './mockData';
+import { useState, useEffect } from 'react';
+
+interface Entry {
+  id: number;
+  value: number;
+  description: string;
+}
 
 function App() {
+  const [data, setData] = useState<Entry[]>();
+
+  useEffect(() => {
+    setData(mockData);
+  }, []);
+
   return (
     <div
       style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
@@ -12,13 +25,17 @@ function App() {
       Description
       <input />
       <h2>Entry List</h2>
-      {mockData.map((el) => (
-        <div style={{ width: '50%', textAlign: 'center', margin: '1rem 0' }}>
-          {el.description}
-          <br />
-          {el.value}
-        </div>
-      ))}
+      {data ? (
+        data.map((el) => (
+          <div style={{ width: '50%', textAlign: 'center', margin: '1rem 0' }}>
+            {el.description}
+            <br />
+            {el.value}
+          </div>
+        ))
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
