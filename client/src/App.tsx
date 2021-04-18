49fde548e695ad8a6a26f74624d0a83acc82c8b1
Gabriel Nakaema <gsnakaema@gmail.com>
2021-04-18 19:26:02 -0300

    add total sum of entry values

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,10 +11,17 @@ function App() {
   const [data, setData] = useState<Entry[]>();
   const [newValue, setNewValue] = useState('');
   const [newDescription, setNewDescription] = useState('');
+  const [total, setTotal] = useState(0.0);
 
   useEffect(() => {
     setData(mockData);
   }, []);
+
+  useEffect(() => {
+    if (data) {
+      setTotal(data?.reduce((acc, el) => acc + el.value, 0));
+    }
+  }, [data]);
 
   function handleSubmit() {
     if (data) {
@@ -43,6 +50,7 @@ function App() {
       style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
     >
       <h1>Finances Tracker</h1>
+      <h2>Total : {total.toFixed(2)}</h2>
       <h2>Add New Entry</h2>
       Value
       <input
