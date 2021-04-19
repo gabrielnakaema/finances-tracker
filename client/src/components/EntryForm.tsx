e0244ea1855713aea59983fe5bad45a0ba694c93
Gabriel Nakaema <gsnakaema@gmail.com>
2021-04-19 15:01:02 -0300

    add radio inputs for entry type

--- client/src/components/EntryForm.tsx
+++ client/src/components/EntryForm.tsx
@@ -12,10 +12,11 @@ interface EntryFormProps {
 const EntryForm = (props: EntryFormProps) => {
   const [value, setValue] = useState('');
   const [description, setDescription] = useState('');
+  const [isExpense, setIsExpense] = useState(true);
 
   const handleSubmit = () => {
     const newEntry = {
-      value: Number(value),
+      value: isExpense ? -1.0 * Number(value) : Number(value),
       description,
     };
     props.addNewEntry(newEntry);
@@ -26,6 +27,25 @@ const EntryForm = (props: EntryFormProps) => {
   return (
     <>
       <h2>Add New Entry</h2>
+      <div>
+        <input
+          type="radio"
+          value="expense"
+          name="expense-type"
+          checked={isExpense}
+          onClick={() => setIsExpense(true)}
+        />{' '}
+        Expense
+        <input
+          type="radio"
+          value="income"
+          name="expense-type"
+          checked={!isExpense}
+          onClick={() => setIsExpense(false)}
+        />{' '}
+        Income
+      </div>
+      <br />
       Value
       <input
         value={value}
