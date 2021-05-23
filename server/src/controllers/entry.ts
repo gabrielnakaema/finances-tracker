b56fbc4e646a5aa1527af342363dcdd6110b2b18
Gabriel Nakaema <gsnakaema@gmail.com>
2021-05-23 17:27:51 -0300

    add entry creation functionality

--- server/src/controllers/entry.ts
+++ server/src/controllers/entry.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { Entry } from '../models/entry';
+import { RequestWithUserId } from '../utils/middleware';
 
 const getAll = (req: Request, res: Response) => {
   res.send({ message: 'entries getAll controller' });
@@ -7,7 +8,45 @@ const getAll = (req: Request, res: Response) => {
 
 const getOne = () => {};
 
-const create = () => {};
+const create = async (req: RequestWithUserId, res: Response) => {
+  /* 
+    Necessary Fields :
+      - description
+      - value
+      - type
+      - category
+      - createdBy
+  */
+  if (!req.body.description) {
+    return res.status(400).send({ message: 'description missing' });
+  }
+  if (!req.body.value) {
+    return res.status(400).send({ message: 'value missing' });
+  }
+  if (!req.body.type) {
+    return res.status(400).send({ message: 'type missing' });
+  }
+  if (!req.body.category) {
+    return res.status(400).send({ message: 'category missing' });
+  }
+  if (!req.authorizedUserId) {
+    return res.status(500).send({ message: 'internal server error' });
+  }
+  const { description, value, type, category } = req.body;
+  const authorizedUserId = req.authorizedUserId;
+
+  const newEntry = await Entry.create({
+    description,
+    value,
+    type,
+    category,
+    createdBy: authorizedUserId,
+  });
+  if (newEntry) {
+    console.log(newEntry);
+    res.status(200).send(newEntry);
+  }
+};
 
 const update = () => {};
 
