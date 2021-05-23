c083f1c433b6ee3a19ef3ab6de7eae2b89d262af
Gabriel Nakaema <gsnakaema@gmail.com>
2021-05-23 17:11:09 -0300

    add authentication from database

--- server/src/utils/auth.ts
+++ server/src/utils/auth.ts
@@ -1,18 +1,32 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
+import { User } from '../models/user';
 
-export const login = (req: Request, res: Response, next: NextFunction) => {
-  if (req.body.username === 'test' && req.body.password === '12345') {
-    const userId = 1;
-    if (process.env.SECRET) {
-      const token = jwt.sign({ userId }, process.env.SECRET);
-      return res.json({ auth: true, token });
-    } else {
-      res.status(500).send({ message: 'internal server error' });
-    }
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!process.env.SECRET) {
+    return res.status(400).send({ message: 'internal server error' });
   }
-  res.status(401).json({ message: 'authentication failed' });
+  if (!req.body.username) {
+    return res.status(401).send({ message: 'missing username' });
+  }
+  if (!req.body.password) {
+    return res.status(401).send({ message: 'missing password' });
+  }
+  const { username, password } = req.body;
+  const foundUser = await User.findOne({ username });
+  const doesPasswordMatch = await bcrypt.compare(password, foundUser.password);
+
+  if (doesPasswordMatch) {
+    const token = jwt.sign({ userId: foundUser._id }, process.env.SECRET);
+    return res.status(200).send({ auth: true, token });
+  } else {
+    return res.status(401).send({ message: 'wrong credentials' });
+  }
 };
 
 export const hashPassword = async (textPassword: string) => {
