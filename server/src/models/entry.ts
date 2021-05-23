78a8cb71a8ae4feff471eb38538dd9e6f71c49f1
Gabriel Nakaema <gsnakaema@gmail.com>
2021-05-23 09:35:34 -0300

    add mongoose to project
    add controller,model,routes files

---
+++ server/src/models/entry.ts
@@ -0,0 +1,31 @@
+import mongoose from '../mongoose';
+
+const entrySchema = new mongoose.Schema({
+  description: {
+    type: String,
+    required: true,
+    maxlength: 50,
+  },
+  value: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  createdBy: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+});
+
+export const Entry = mongoose.model('entry', entrySchema);
