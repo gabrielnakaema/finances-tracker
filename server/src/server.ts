e370ea3c6d1ac1bc897da7ee857ba1e05ab3f3bc
Gabriel Nakaema <gsnakaema@gmail.com>
2021-05-23 15:08:07 -0300

    fix mongodb authentication error

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,20 +5,29 @@ import { entriesRouter } from './routes/entries';
 import { login } from './utils/auth';
 
 dotenv.config();
+if (
+  process.env.MONGO_INITDB_ROOT_USERNAME &&
+  process.env.MONGO_INITDB_ROOT_PASSWORD
+) {
+  mongoose.connect('mongodb://localhost:27017/finances', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    authSource: 'admin',
+    user: process.env.MONGO_INITDB_ROOT_USERNAME,
+    pass: process.env.MONGO_INITDB_ROOT_PASSWORD,
+  });
 
-mongoose.connect(
-  `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@localhost:27017/test`,
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
+  const db = mongoose.connection;
 
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'db connection error'));
-db.once('open', () => {
-  console.log('connected to mongoDB');
-});
+  db.on('error', console.error.bind(console, 'db connection error'));
+  db.once('open', () => {
+    console.log('connected to mongoDB');
+  });
+}
 
 const app = express();
+
+app.use(express.json());
 
 app.use('/entries', entriesRouter);
 
