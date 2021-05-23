10516a5fa2f27146d6455a77fdb1add5240982d4
Gabriel Nakaema <gsnakaema@gmail.com>
2021-05-23 15:26:55 -0300

    add timestamps to schema

--- server/src/models/entry.ts
+++ server/src/models/entry.ts
@@ -1,31 +1,34 @@
 import mongoose from 'mongoose';
 
-const entrySchema = new mongoose.Schema({
-  description: {
-    type: String,
-    required: true,
-    maxlength: 50,
+const entrySchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+      maxlength: 50,
+    },
+    value: {
+      type: Number,
+      required: true,
+    },
+    date: {
+      type: Date,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'user',
+      required: true,
+    },
   },
-  value: {
-    type: Number,
-    required: true,
-  },
-  date: {
-    type: Date,
-  },
-  type: {
-    type: String,
-    required: true,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  createdBy: {
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: 'user',
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 export const Entry = mongoose.model('entry', entrySchema);
--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,26 +1,29 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  username: {
-    type: String,
-    required: true,
-    maxlength: 50,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  entries: [
-    {
-      type: mongoose.SchemaTypes.ObjectId,
-      ref: 'entry',
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-  ],
-});
+    username: {
+      type: String,
+      required: true,
+      maxlength: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    entries: [
+      {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'entry',
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 export const User = mongoose.model('user', userSchema);
