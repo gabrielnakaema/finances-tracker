0080d613be9a296a766480163fbc9e518011b9d4
Gabriel Nakaema <gsnakaema@gmail.com>
2021-05-22 23:26:03 -0300

    add separate server file from index

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,3 @@
-import express from 'express';
+import { start } from './server';
 
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('hello wor');
-});
-
-app.listen(3333, () => {
-  console.log('app running on port 3333');
-});
+start();
