18a778533a89a1a0219947a30b3eed3a1fb83fc6
Gabriel Nakaema <gsnakaema@gmail.com>
2021-05-24 14:56:22 -0300

    simplify request  params checking

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -5,14 +5,11 @@ import { hashPassword } from '../utils/auth';
 const getOne = () => {};
 
 export const create = async (req: Request, res: Response) => {
-  if (!req.body.username) {
-    return res.status(400).send({ message: 'missing username' });
-  }
-  if (!req.body.password) {
-    return res.status(400).send({ message: 'missing password' });
-  }
-  if (!req.body.name) {
-    return res.status(400).send({ message: 'missing person name' });
+  const requiredFields = ['username', 'password', 'name'];
+  for (const field of requiredFields) {
+    if (!req.body[field]) {
+      return res.status(400).send({ message: `missing param: ${field}` });
+    }
   }
   const { username, password, name } = req.body;
   const hashedPassword = await hashPassword(password);
