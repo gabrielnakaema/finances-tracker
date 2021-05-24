6ebc527edbbc09bec64de26157777bd444f2921c
Gabriel Nakaema <gsnakaema@gmail.com>
2021-05-24 15:03:09 -0300

    add middleware to check userId in requests

--- server/src/controllers/entry.ts
+++ server/src/controllers/entry.ts
@@ -3,9 +3,6 @@ import { Entry } from '../models/entry';
 import { RequestWithUserId } from '../utils/middleware';
 
 const getAll = async (req: RequestWithUserId, res: Response) => {
-  if (!req.authorizedUserId) {
-    return res.status(500).send({ message: 'internal server error' });
-  }
   const authorizedUserId = req.authorizedUserId;
   const entries = await Entry.find({ createdBy: authorizedUserId });
   res.status(200).send(entries);
@@ -19,9 +16,6 @@ const create = async (req: RequestWithUserId, res: Response) => {
     if (!req.body[field]) {
       return res.status(400).send({ message: `missing param: ${field}` });
     }
-  }
-  if (!req.authorizedUserId) {
-    return res.status(500).send({ message: 'internal server error' });
   }
   const { description, value, type, category } = req.body;
   const authorizedUserId = req.authorizedUserId;
