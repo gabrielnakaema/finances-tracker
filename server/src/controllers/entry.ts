4290d3776074d1d495031409c67447b188a7824a
Gabriel Nakaema <gsnakaema@gmail.com>
2021-05-24 14:56:22 -0300

    simplify request  params checking

--- server/src/controllers/entry.ts
+++ server/src/controllers/entry.ts
@@ -14,25 +14,11 @@ const getAll = async (req: RequestWithUserId, res: Response) => {
 const getOne = () => {};
 
 const create = async (req: RequestWithUserId, res: Response) => {
-  /* 
-    Necessary Fields :
-      - description
-      - value
-      - type
-      - category
-      - createdBy
-  */
-  if (!req.body.description) {
-    return res.status(400).send({ message: 'description missing' });
-  }
-  if (!req.body.value) {
-    return res.status(400).send({ message: 'value missing' });
-  }
-  if (!req.body.type) {
-    return res.status(400).send({ message: 'type missing' });
-  }
-  if (!req.body.category) {
-    return res.status(400).send({ message: 'category missing' });
+  const requiredFields = ['description', 'value', 'type', 'category'];
+  for (const field of requiredFields) {
+    if (!req.body[field]) {
+      return res.status(400).send({ message: `missing param: ${field}` });
+    }
   }
   if (!req.authorizedUserId) {
     return res.status(500).send({ message: 'internal server error' });
@@ -48,7 +34,6 @@ const create = async (req: RequestWithUserId, res: Response) => {
     createdBy: authorizedUserId,
   });
   if (newEntry) {
-    console.log(newEntry);
     res.status(200).send(newEntry);
   }
 };
