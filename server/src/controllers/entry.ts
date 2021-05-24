7efafb124708cd884ee82b6170ec5d0f28ef2eac
Gabriel Nakaema <gsnakaema@gmail.com>
2021-05-24 16:00:06 -0300

    add get one entry endpoint, function

--- server/src/controllers/entry.ts
+++ server/src/controllers/entry.ts
@@ -2,15 +2,35 @@ import { Response } from 'express';
 import { Entry } from '../models/entry';
 import { RequestWithUserId } from '../utils/middleware';
 
-const getAll = async (req: RequestWithUserId, res: Response) => {
+const getAll = async (req: RequestWithUserId, res: Response): Promise<void> => {
   const authorizedUserId = req.authorizedUserId;
   const entries = await Entry.find({ createdBy: authorizedUserId });
   res.status(200).send(entries);
 };
 
-const getOne = () => {};
+const getOne = async (
+  req: RequestWithUserId,
+  res: Response
+): Promise<Response | void> => {
+  const authorizedUserId = req.authorizedUserId;
+  if (!req.params.id) {
+    return res.status(400).send({ message: 'missing param: id' });
+  }
+  const foundEntry = await Entry.findOne({
+    createdBy: authorizedUserId,
+    _id: req.params.id,
+  });
+  if (foundEntry) {
+    res.status(200).send(foundEntry);
+  } else {
+    res.status(404).send({ message: 'entry not found' });
+  }
+};
 
-const create = async (req: RequestWithUserId, res: Response) => {
+const create = async (
+  req: RequestWithUserId,
+  res: Response
+): Promise<Response | void> => {
   const requiredFields = ['description', 'value', 'type', 'category'];
   for (const field of requiredFields) {
     if (!req.body[field]) {
