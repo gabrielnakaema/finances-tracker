04d903596cdbdd8e79794f0430d498e1cbde7947
Gabriel Nakaema <gsnakaema@gmail.com>
2021-05-30 18:07:04 -0300

    change text input component styles

--- client/src/components/TextInput.tsx
+++ client/src/components/TextInput.tsx
@@ -12,12 +12,17 @@ interface TextInputProps {
 
 const TextInput = (props: TextInputProps) => {
   return (
-    <div>
-      <label htmlFor={props.inputId}>{props.labelText}</label>
-      <br />
+    <div className="m-3">
+      <label
+        className="block text-gray-700 font-bold mb-2"
+        htmlFor={props.inputId}
+      >
+        {props.labelText}
+      </label>
       <input
+        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight
+                  focus:outline-none focus:bg-white focus:border-blue-500"
         id={props.inputId}
-        className={props.className}
         type={props.type}
         min={props.min}
         value={props.value}
