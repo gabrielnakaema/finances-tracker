6d17637245d522fdf716b467bb238a571334fbf0
Gabriel Nakaema <gsnakaema@gmail.com>
2021-06-16 12:40:20 -0300

    add sign up link

--- client/src/components/SignInForm.tsx
+++ client/src/components/SignInForm.tsx
@@ -4,6 +4,7 @@ import { validateUsername, validatePassword } from '../utils/validation';
 import { useTextField } from '../hooks/useTextField';
 import { useContext } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
+import { Link } from 'react-router-dom';
 
 const SignInForm = () => {
   const username = useTextField('text', validateUsername);
@@ -12,13 +13,11 @@ const SignInForm = () => {
 
   const handleSubmit = async (e: React.SyntheticEvent<HTMLFormElement>) => {
     e.preventDefault();
-
     const isUsernameOk = !username.error && username.callValidation();
     const isPasswordOk = !password.error && password.callValidation();
     if (!isUsernameOk || !isPasswordOk) {
       return;
     }
-
     await authContext.signIn(username.value, password.value);
   };
 
@@ -43,13 +42,20 @@ const SignInForm = () => {
           inputId="password-login-input"
           error={password.error}
         />
+
         <button
           type="submit"
-          className="bg-blue-500 p-3 hover:bg-blue-300 text-white rounded w-1/2 block mx-auto font-medium"
+          className="bg-blue-500 p-3 hover:bg-blue-300 text-white rounded w-1/2 block font-medium mx-auto"
         >
           Sign In
         </button>
       </form>
+      <div className="text-center mt-5">
+        <span className=" text-gray-700">Don't have an account ? </span>
+        <Link className="text-blue-500 underline" to="/signup">
+          Sign Up!
+        </Link>
+      </div>
     </div>
   );
 };
