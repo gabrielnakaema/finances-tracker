16683e5a0fac0074e9f927be0f96990a7e69ea3d
Gabriel Nakaema <gsnakaema@gmail.com>
2021-06-25 17:19:43 -0300

    change text styling

--- client/src/components/ErrorMessage.tsx
+++ client/src/components/ErrorMessage.tsx
@@ -1,13 +1,16 @@
-interface ErrorComponentProps {
+interface ErrorMessageProps {
   message: string;
 }
 
-const ErrorComponent = (props: ErrorComponentProps) => {
+const ErrorMessage = (props: ErrorMessageProps) => {
   return (
-    <div hidden={props.message ? false : true} className="text-red-500 bg-red-100 border-red-500 border rounded m-3 py-2 px-4">
+    <div
+      hidden={props.message ? false : true}
+      className="capitalize text-red-500 bg-red-100 border-red-500 border rounded m-3 py-2 px-4"
+    >
       {props.message}
     </div>
   );
 };
 
-export default ErrorComponent;
+export default ErrorMessage;
