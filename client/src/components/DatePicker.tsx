415f98f9fa49f2987d444be2421f77a5813eb98c
Gabriel Nakaema <gsnakaema@gmail.com>
2021-07-01 22:13:24 -0300

    add popperPlacement property to DatePicker
    fixes triangle appearing out of input's center

--- client/src/components/DatePicker.tsx
+++ client/src/components/DatePicker.tsx
@@ -5,10 +5,6 @@ interface CustomDatePickerProps {
   onChange: (date: Date) => void;
 }
 
-// TODO
-// Fix Datepicker's arrow above input field to be responsive to input field width,
-// maybe also center it on input instead of it being anchored to the left
-
 const CustomDatePicker = (props: CustomDatePickerProps) => {
   return (
     <div className="m-3">
@@ -22,6 +18,7 @@ const CustomDatePicker = (props: CustomDatePickerProps) => {
       <DatePicker
         id="date-picker-input"
         selected={props.date}
+        popperPlacement="top"
         onChange={props.onChange}
         className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight
   focus:outline-none focus:bg-white focus:border-blue-500"
